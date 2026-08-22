67ff8e7413e5f212acd9ec7a1795044b3b07ef46
georgeabdallah82 <georgeabdallah82@gmail.com>
2026-08-23 01:45:05 +0300

    Use Shopify-style theme editor

--- app/admin/online-store/theme-editor/page.tsx
+++ app/admin/online-store/theme-editor/page.tsx
@@ -1,4 +1,8 @@
 import {requirePermission} from '@/lib/auth'
 import {getThemeState} from '@/lib/theme'
-import ThemeEditor from '@/components/theme-editor'
-export default async function ThemeEditorPage(){await requirePermission('content.view');return <ThemeEditor initial={JSON.parse(JSON.stringify(await getThemeState()))}/>} 
+import ShopifyThemeEditor from '@/components/shopify-theme-editor'
+
+export default async function ThemeEditorPage(){
+  await requirePermission('content.view')
+  return <ShopifyThemeEditor initial={JSON.parse(JSON.stringify(await getThemeState()))}/>
+}
