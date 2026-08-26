58e2c62ff6ec9bb107d9c2c7798dea0a528ebc57
georgeabdallah82 <georgeabdallah82@gmail.com>
2026-08-26 10:25:24 +0300

    Make Focal editor the active theme editor

--- components/shopify-theme-editor-page.tsx
+++ components/shopify-theme-editor-page.tsx
@@ -1,8 +1,7 @@
-import ShopifyThemeEditorV3 from '@/components/shopify-theme-editor-v3'
-import ThemeInspectorStyles from '@/components/theme-inspector-styles'
+import FocalThemeEditor from '@/components/focal-theme-editor'
 
 type Props = { initial: { theme: Record<string, any>; sections: any[]; navigation: any[] } }
 
 export default function ShopifyThemeEditorPage({ initial }: Props) {
-  return <><ThemeInspectorStyles /><ShopifyThemeEditorV3 initial={initial} /></>
+  return <FocalThemeEditor initial={initial} />
 }
