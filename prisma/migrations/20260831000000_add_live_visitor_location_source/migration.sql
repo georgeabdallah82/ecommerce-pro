e741f7bad9ced05da2e89cd7f2595175acdfe846
georgeabdallah82 <georgeabdallah82@gmail.com>
2026-09-01 00:21:45 +0300

    db: replace location source migration with legacy coordinate cleanup

--- prisma/migrations/20260831000000_add_live_visitor_location_source/migration.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "LiveVisitorSession"
-  ADD COLUMN IF NOT EXISTS "locationSource" TEXT;
-
-CREATE INDEX IF NOT EXISTS "LiveVisitorSession_locationSource_idx"
-  ON "LiveVisitorSession"("locationSource");
