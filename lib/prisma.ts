7e518e3e40f52ed76dee1c2cee3afbab761b4230
georgeabdallah82 <georgeabdallah82@gmail.com>
2026-09-06 17:14:13 +0300

    Use Prisma 6 edge client with Accelerate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,18 @@
-import { PrismaClient } from '@prisma/client'
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
-export const db = globalForPrisma.prisma ?? new PrismaClient()
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: ReturnType<typeof createPrismaClient>
+}
+
+function createPrismaClient() {
+  const databaseUrl = process.env.DATABASE_URL
+  if (!databaseUrl) throw new Error('DATABASE_URL is required')
+
+  return new PrismaClient({
+    accelerateUrl: databaseUrl,
+  }).$extends(withAccelerate())
+}
+
+export const db = globalForPrisma.prisma ?? createPrismaClient()
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
